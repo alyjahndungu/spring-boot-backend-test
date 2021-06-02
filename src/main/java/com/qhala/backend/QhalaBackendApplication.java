27d0aeb54d90cf1bf447bf60a9e07c123136397a
elijah-systems <elijahndungu30@gmail.com>
2021-06-02 15:50:16 +0300

    first commit

---
+++ src/main/java/com/qhala/backend/QhalaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qhala.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QhalaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QhalaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qhala/backend/QhalaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qhala.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QhalaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
